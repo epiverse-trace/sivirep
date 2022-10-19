77419dac817406a27a168b5bddbcc73c26e651e8
Geraldine <geraldinegomez070@gmail.com>
2022-10-19 06:36:46 -0500

    cleaning and checking function creation

--- R/read_data/import_data.R
+++ R/read_data/import_data.R
@@ -1,39 +1,48 @@
-library(dplyr)
-library(tidyverse)
-library(config)
+config   <- config::get()
+temp_dir <- tempdir()
 
+#' Import Path Data
+#' 
 #' Función que importa la informacion de SIVIGILA a través de una URL
 #' Function that imports SIVIGILA data through a URL
 #' @param url_data URL of SIVIGILA data
 #' @return The data downloaded in csv format
 #' @examples
-#' import_sivigila_data("https://www.datos.gov.co/api/views/qvnt-2igj/rows.csv?accessType=DOWNLOAD")
-import_sivigila_data <- function(url_data) {
+#' import_path_data("https://www.datos.gov.co/api/views/qvnt-2igj/rows.csv?accessType=DOWNLOAD")
+#' @export
+import_path_data <- function(url_data, cache = TRUE) {
   data <- read.csv(url_data)
   return(data)
 }
 
+#' Import Data Delim
+#' 
 #' Función que importa la informacion del SIVIGILA y la tabula a traves de un delimitador
 #' Function that imports the SIVIGILA information and tabulates it through a delimiter
 #' @param path_data Path of SIVIGILA data
 #' @return Data tabulated
 #' @examples
 #' import_data_delim(https://www.datos.gov.co/api/views/qvnt-2igj/rows.csv?accessType=DOWNLOAD")
+#' @export
 import_data_delim <- function(path_data) {
-  data <- if ("|" %in% strsplit(readLines(path_data, n = 1)[1], split = "")[[1]] ) { 
-            fread(path_data, sep = "|")  
-        }else if ("," %in% strsplit(readLines(path_data, n = 1)[1], split = "")[[1]] ){
-            fread(path_data, sep = ",")
-        }else if (";" %in% strsplit(readLines(path_data, n = 1)[1], split = "")[[1]] ){
-            fread(path_data, sep = ";")
-        }else if (":" %in% strsplit(readLines(path_data, n = 1)[1], split = "")[[1]] ){
-          fread(path_data, sep = ":")
-        }else {
-          fread(path_data)
+  delims <- config::get("data_delim")
+  data <- data.frame()
+  for (delim in delims) {
+       if (delim %in% strsplit(readLines(path_data, n = 1)[1], split = "")[[1]] ) { 
+            data <- data.table::fread(path_data, sep = delim)
+            break;
         }
+  }
+  
+  if (empty(data)) {
+       data <- data.table::fread(path_data)
+  }
+  
   return(data)
 }
 
+#' Import Data Endemic Channel
+#' 
 #' Función que importa la informacion del SIVIGILA para la construcción del canal endémico
 #' Function that imports SIVIGILA for building the endemic channel
 #' @param path_data Path of SIVIGILA data
@@ -42,6 +51,7 @@ import_data_delim <- function(path_data) {
 #' @return The 5 years data of a disease
 #' @examples
 #' import_data_endemic_channel("MALARIA", 2020)
+#' @export
 import_data_endemic_channel <- function(disease_name, year) {
   file_path <- "../data/malaria/complicada/datos_"
   initial_year <- year - 4
@@ -60,26 +70,29 @@ import_data_endemic_channel <- function(disease_name, year) {
   return(disease_data_by_years)
 }
 
+#' Import Avaliable Diseases and Years
+#' 
 #' Función que obtiene las enfermedades y los años disponibles de los microdatos de SIVIGILA
 #' Function that obtains the diseases and the years available from the SIVIGILA microdata
 #' @return The diseases and the years available
 #' @examples 
-#' get_avaliable_diseases_and_years()
+#' import_avaliable_diseases_and_years()
+#' @export
 import_avaliable_diseases_and_years <- function()  {
   
-  query_diseases_by_year_path <- "https://portalsivigila.ins.gov.co/_api/web/lists/GetByTitle('Microdatos')/items?$select=Evento,A_x00f1_o,NombreEvento&$orderby=NombreEvento%20asc&$top=1000"
-  get_query_diseases_by_year  <- httr::GET(query_diseases_by_year_path, add_headers("Accept" = "*/*"))
+  query_diseases_by_year_path <- config::get("query_diseases_by_year_path")
+  get_query_diseases_by_year  <- httr::GET(query_diseases_by_year_path, xml2::add_headers("Accept" = "*/*"))
   
-  content_type_response <- str_split_fixed(headers(get_query_diseases_by_year)$`content-type`, pattern = ";", 3)
-  content_type_response <- str_replace(content_type_response[[1]], "atom\\+", "")
-  query_diseases_by_year_content <- content(get_query_diseases_by_year, type = content_type_response, encoding = "UTF-8")
+  content_type_response <- xml2::str_split_fixed(xml2::headers(get_query_diseases_by_year)$`content-type`, pattern = ";", 3)
+  content_type_response <- xml2::str_replace(xml2::content_type_response[[1]], "atom\\+", "")
+  query_diseases_by_year_content <- xml2::content(get_query_diseases_by_year, type = content_type_response, encoding = "UTF-8")
   
   
-  children      <- xml_children(query_diseases_by_year_content)
-  children      <- xml_children(children)
-  children      <- xml_children(children)
-  children      <- xml_children(children)
-  children_text <- xml_text(children)
+  children      <- xml2::xml_children(query_diseases_by_year_content)
+  children      <- xml2::xml_children(children)
+  children      <- xml2::xml_children(children)
+  children      <- xml2::xml_children(children)
+  children_text <- xml2::xml_text(children)
   
   data_avaliable <- data.frame( 
     disease = unique(children_text),
@@ -96,35 +109,47 @@ import_avaliable_diseases_and_years <- function()  {
   return(data_avaliable_diseases_and_years)
 }
 
+#' Import Data of a Disease By Year
+#' 
 #' Función que obtiene los datos de la enfermedad por año
 #' Function that obtains the disease data by year
 #' @param year The year
 #' @param disease_name The disease name
 #' @return The disease data by year
 #' @examples
-#' get_data_disease_by_year(2018, "DENGUE")
-import_data_disease_by_year <- function(year, disease_name, cache = TRUE, use_cache_file = TRUE) {
-  
+#' import_data_disease_by_year(2018, "DENGUE")
+#' @export
+import_data_disease_by_year <- function(year, disease_name, cache = TRUE) {
   data_url <- get_path_data_disease_by_year(year, disease_name)
   data_disease_by_year <- data.frame()
-  data_file_name <- find_name_file_path(data_url)
+  data_file_name <- paste0(temp_dir, "/", find_name_file_path(data_url))
   
-  if (!use_cache_file) {
-      data_disease_by_year <- import_data_delim(data_url)
-      if (cache) {
-          write.csv(data_disease_by_year, file = paste0("../data/", data_file_name)) 
+  if (cache) {
+      if (file.exists(data_file_name)) {
+           data_disease_by_year <- read.csv(file = data_file_name) 
+      }
+      else{
+           data_disease_by_year <- import_data_delim(data_url)
+           write.csv(data_disease_by_year, file = data_file_name) 
       }
   }
   else {
-    data_disease_by_year <- read.csv(file = paste0("../data/", data_file_name))
+      data_disease_by_year <- import_data_delim(data_url)
   }
-  
   return(data_disease_by_year)
 }
 
-
+#' Find Name File Path or URL
+#' 
+#' Función que obtiene el nombre del archivo a descargar desde una URL o ruta
+#' Function that obtains the name of the file to download from a URL or path
+#' @param path The path or URL
+#' @return The name file
+#' @examples
+#' find_name_file_path("DENGUE")
+#' @export
 find_name_file_path <- function(path) {
-  name_file <- strsplit(path, "/Microdatos/")
+  name_file <- strsplit(path, config::get("name_file_split"))
   name_file <- strsplit(name_file[[1]][2], "')")[[1]][1] %>% as.character()
   return(name_file)
 }
--- R/read_data/scraping_data.R
+++ R/read_data/scraping_data.R
@@ -1,7 +1,15 @@
-library(config)
-
 config <- config::get()
 
+#' Get Path Data Disease by Year
+#' 
+#' Función que obtiene la ruta o URL del servicio para descargar la informacion de una enfermedad por año desde SIVIGILA
+#' Function that obtains the path or URL of the service to download the information of a disease per year from SIVIGILA
+#' @param year The year
+#' @param disease_name The disease name
+#' @return Path or URL for download the disease data by selected year
+#' @examples
+#' get_path_data_disease_by_year(2010, "DENGUE")
+#' @export
 get_path_data_disease_by_year <- function(year, disease_name) {
   base_path <- config::get("base_path_microdata")
   file_path_parameters <- config::get("file_path_parameters_microdata")
@@ -11,32 +19,31 @@ get_path_data_disease_by_year <- function(year, disease_name) {
   
   year <- as.character(year)
   
-  disease_name <- str_replace_all(string = disease_name, pattern = " ", repl = "%20")
-  query_path   <- str_replace(query_path, "_year_", year)
-  query_path   <- str_replace(query_path, "_disease_", disease_name)
+  disease_name <- stringr::str_replace_all(string = disease_name, pattern = " ", repl = "%20")
+  query_path   <- stringr::str_replace(query_path, "_year_", year)
+  query_path   <- stringr::str_replace(query_path, "_disease_", disease_name)
   
   query_disease_path <- paste(base_path, paste(microdata_path, query_path, sep = ""), sep = "")
-  get_query_disease <- httr::GET(query_disease_path, add_headers("Accept" = "*/*"))
+  get_query_disease <- httr::GET(query_disease_path, xml2::add_headers("Accept" = "*/*"))
   
-  content_type_response <- str_split_fixed(headers(get_query_disease)$`content-type`, pattern = ";", 3)
-  content_type_response <- str_replace(content_type_response[[1]], "atom\\+", "")
-  query_disease_content <- content(get_query_disease, type = content_type_response, encoding = "UTF-8")
+  content_type_response <- stringr::str_split_fixed(xml2::headers(get_query_disease)$`content-type`, pattern = ";", 3)
+  content_type_response <- stringr::str_replace(content_type_response[[1]], "atom\\+", "")
+  query_disease_content <- xml2::content(get_query_disease, type = content_type_response, encoding = "UTF-8")
   
-  entry_nodes_query_disease_content <- xml_child(query_disease_content, 4)
-  edit_node_query_disease_content   <- xml_child(entry_nodes_query_disease_content, 12)
-  list_file_path <- xml_attr(edit_node_query_disease_content, "href")[1]
+  entry_nodes_query_disease_content <- xml2::xml_child(query_disease_content, 4)
+  edit_node_query_disease_content   <- xml2::xml_child(entry_nodes_query_disease_content, 12)
+  list_file_path <- xml2::xml_attr(edit_node_query_disease_content, "href")[1]
   
-  get_info_file <- httr::GET(paste(base_path, list_file_path,  sep = ""), add_headers("Accept" = "*/*"))
+  get_info_file <- httr::GET(paste(base_path, list_file_path,  sep = ""), xml2::add_headers("Accept" = "*/*"))
   
-  content_type_response <- str_split_fixed(headers(get_info_file)$`content-type`, pattern = ";", 3)
-  content_type_response <- str_replace(content_type_response[[1]], "atom\\+", "")
-  info_file_content <- content(get_info_file, type = content_type_response, encoding = "UTF-8")
+  content_type_response <- stringr::str_split_fixed(xml2::headers(get_info_file)$`content-type`, pattern = ";", 3)
+  content_type_response <- stringr::str_replace(content_type_response[[1]], "atom\\+", "")
+  info_file_content <- xml2::content(get_info_file, type = content_type_response, encoding = "UTF-8")
   
-  edit_node_info_file_content <- xml_child(info_file_content, 3)
-  file_path <- xml_attr(edit_node_info_file_content, "href")[1]
+  edit_node_info_file_content <- xml2::xml_child(info_file_content, 3)
+  file_path <- xml2::xml_attr(edit_node_info_file_content, "href")[1]
   
   file_download_path <- paste(base_path, paste(file_path, file_path_parameters, sep = ""), sep = "")
-  file_download_path
-  
+
   return(file_download_path)
 }
