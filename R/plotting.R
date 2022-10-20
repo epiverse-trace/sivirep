449326ef925743b8c2116359685b49440df9af45
GeraldineGomez <46997659+GeraldineGomez@users.noreply.github.com>
2022-10-20 05:52:38 -0500

    refactoring import functions of open data and get deptos

--- R/plotting.R
+++ R/plotting.R
@@ -1,9 +1,16 @@
-if (!require(gpclib)) 
+#' @import tidyverse
+#' @import aweek
+#' @import ggspatial
+#' @import rgdal
+#' @import maptools
+
+if (!require(gpclib))
   install.packages("gpclib", type = "source")
 maptools::gpclibPermit()
 
+
 #' Plot epiweek
-#' 
+#'
 #' Función que genera el reporte de la Semana epidemiológica
 #' Function that generates the report of the Epidemiological Week
 #' @param data Disease data
@@ -21,7 +28,7 @@ plot_epiweek <- function(dat, var_week, var_cases, year, type = "week", xlabel =
   dat$epiweek <- dat[,var_week]
   dat$cases_count <- dat[,var_cases]
   dat_plot <- dat %>% dplyr::group_by(epiweek, Nombre) %>% dplyr::summarise(casos = sum(cases_count), .groups = "drop")
-  
+
   if (type == "week") {
       plot <- ggplot2::ggplot(dat_plot) +
         ggplot2::geom_col(ggplot2::aes(x = epiweek, y = casos, fill = Nombre), alpha = 0.9) +
@@ -30,9 +37,9 @@ plot_epiweek <- function(dat, var_week, var_cases, year, type = "week", xlabel =
         ggplot2::scale_fill_discrete(name = "") +
         ggplot2::theme(legend.position = "bottom")
   }
-  
+
   if (type == "date") {
-    
+
     dat_plot$date_week <- as.Date(paste(year, dat_plot$epiweek, 1, sep = "-"), "%Y-%U-%u")
     plot <- ggplot2::ggplot(dat_plot) +
       ggplot2::geom_col(ggplot2::aes(x = date_week, y = casos, fill = Nombre), alpha = 0.9) +
@@ -41,12 +48,12 @@ plot_epiweek <- function(dat, var_week, var_cases, year, type = "week", xlabel =
       ggplot2::scale_fill_discrete(name = "") +
       ggplot2::theme(legend.position = "bottom")
   }
-  
+
   return(plot)
 }
 
 #' Plot Department Map by Selected Disease
-#' 
+#'
 #' Función que genera el mapa por departamento de la información de la enfermedad seleccionada
 #' Function that generates the map by information department of the selected disease
 #' @param data_map_depto Data for department
@@ -55,30 +62,30 @@ plot_epiweek <- function(dat, var_week, var_cases, year, type = "week", xlabel =
 #' plot_dept_map(data_map_disease_deptos, var_lj = "id")
 #' @export
 plot_dept_map <- function(data_map_depto, var_lj = "id") {
-  
+
   shp <- rgdal::readOGR(dsn = file.path("../data/depto_adm_shp/depto.shp"), stringsAsFactors = FALSE)
   shp.df <- ggplot2::fortify(shp, region = "DPTO")
-  shp.df <- shp.df %>% 
+  shp.df <- shp.df %>%
     dplyr::left_join(data_map_depto, by = var_lj)
-  
-  map <- ggplot2::ggplot() + 
-    ggplot2::geom_polygon(data = shp.df, ggplot2::aes(x = long, y = lat, group = group, fill = casos), 
+
+  map <- ggplot2::ggplot() +
+    ggplot2::geom_polygon(data = shp.df, ggplot2::aes(x = long, y = lat, group = group, fill = casos),
                  colour = "black") +
     ggplot2::scale_fill_gradient(low = "white", high = "darkred") +
     ggplot2::theme_void()
-  
+
   return(map)
 }
 
 #' Plot by Variable
-#' 
+#'
 #' Función que genera gráficas por cualquier tipo de variable
 #' Function that generates graphs by any type of variable
 #' @param data The data to plot
 #' @param var_x The variable name for axis x
 #' @param var_y The variable name for axis y
 #' @param var_fill The variable fill
-#' @param wt_per Indicates if the data have percentages 
+#' @param wt_per Indicates if the data have percentages
 #' @param label_x The label for axis x
 #' @param label_y The label for axis y
 #' @param scale_name The scale name
@@ -91,14 +98,14 @@ plot_dept_map <- function(data_map_depto, var_lj = "id") {
 #' @examples
 #' plot_by_variable(disease_dt_by_gender_wk, var_x = "SEMANA", var_y = "Casos", var_fill = "SEXO", var_per = "Porcentaje", label_x = "Género", label_y = "Casos \n (Porcentajes)", scale_name = "Género", scale_labels = c("Femenino", "Masculino"), diagram_title = "Diagrama para la variable Sexo", legend_pos = "right", bar_wd = 1, text_sz = 0.5)
 #' @export
-plot_by_variable <- function(data, var_x, var_y, var_per = NULL, var_fill, wt_per = TRUE, label_x, label_y, 
+plot_by_variable <- function(data, var_x, var_y, var_per = NULL, var_fill, wt_per = TRUE, label_x, label_y,
                              scale_name = NULL, scale_labels = NULL, diagram_title, legend_pos, bar_wd = 1, text_sz, show_val = TRUE) {
-  ggplot2::ggplot(data, ggplot2::aes_string(x = var_x, y = var_y, fill = var_fill) ) +  
-    ggplot2::geom_bar(width = bar_wd, stat = "identity", position = ggplot2::position_dodge()) + 
+  ggplot2::ggplot(data, ggplot2::aes_string(x = var_x, y = var_y, fill = var_fill) ) +
+    ggplot2::geom_bar(width = bar_wd, stat = "identity", position = ggplot2::position_dodge()) +
     ggplot2::labs(x = label_x, y = label_y) +
     ggplot2::labs(fill = "") +
     ggplot2::theme_classic() +
-    {if (show_val) 
+    {if (show_val)
          ggplot2::geom_text(
                 {if (!is.null(var_per)) eval(parse(text = paste0("ggplot2::aes(label = paste0(", var_y,", '\n (' ,", var_per, ", '%', ')'","))")))
                  else eval(parse(text = paste0("ggplot2::aes(label = ",var_y,")")))},
@@ -108,7 +115,7 @@ plot_by_variable <- function(data, var_x, var_y, var_per = NULL, var_fill, wt_pe
                 position = ggplot2::position_dodge(0.9),
                 angle = 0,
                 size = text_sz,
-        ) 
+        )
     } +
     # ggplot2::scale_fill_discrete(name = scale_name, {if (!is.null(scale_labels)) labels = scale_labels}) +
     # theme(axis.text.x = element_text(angle = -45, vjust = 1, hjust = -0.3)) +
