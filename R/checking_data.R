36a7f2a4341fa49fc80f93144d8bb70e82ee9d23
GeraldineGomez <46997659+GeraldineGomez@users.noreply.github.com>
2022-10-21 14:19:08 -0500

    modify vignettes and get dept codes func

--- R/checking_data.R
+++ R/checking_data.R
@@ -25,7 +25,7 @@ filter_disease  <- function(name_disease, sivigila_summary_data) {
 #' get_depto_codes(geo_codes)
 #' @export
 get_depto_codes <- function(geo_codes) {
-  deptos_data   <- geo_codes %>% dplyr::group_by(cod_dep = .data$Código.Departamento, name_dep = .data$Nombre.Departamento) %>%
+  deptos_data   <- geo_codes %>% dplyr::group_by(cod_dep = .data$Departamento, name_dep = .data$Departamento) %>%
     dplyr::select(.data$cod_dep, .data$name_dep) %>% dplyr::distinct()
   deptos_data   <- deptos_data[1:33,]
   return(deptos_data)
