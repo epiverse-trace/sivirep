78693a142e590c30184336d4512a48100ce1f342
GeraldineGomez <avocadoenterprise@gmail.com>
2023-12-01 01:57:47 -0500

    feat(divipola_data): created data set with codes and geo info of DIVIPOLA
    This commit includes:
    
    - Creation of RData files and xlsx
    - Creation of R file description of dataset

---
+++ R/divipoladata.R
@@ -0,0 +1,15 @@
+#' Códigos e información geografica del DIVIPOLA en sivirep
+#'
+#' Data from API de Datos Abiertos de Colombia
+#'
+#' @docType data
+#'
+#' @usage divipoladata
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' divipoladata
+"divipoladata"
