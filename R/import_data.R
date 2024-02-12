44536c4edf6ba6e223514b71e41ebf4c11ef1c3b
GeraldineGomez <avocadoenterprise@gmail.com>
2024-02-12 12:10:57 -0500

    fix(impor_data_event): adding extra validation for dengue 2016
    Created extra validation for cod_eve column in the data that provides SIVIGILA source because, it appears duplicated in Dengue 2016
    
    Refs: #90

--- R/import_data.R
+++ R/import_data.R
@@ -171,6 +171,15 @@ import_data_event <- function(nombre_event,
       data_import$fec_def <- as.character(data_import$fec_def)
       nombre_cols <- names(data_import)
       nombre_cols <- nombre_cols[-which(nombre_cols %in% cols_remover)]
+      index_cols_eve <- which(stringr::str_detect(nombre_cols,
+                                                  stringr::fixed("cod_eve_"))
+                              %in% TRUE)
+      if (!identical(index_cols_eve,
+                     integer(0))) {
+        names(data_import)[index_cols_eve[1]] <- "cod_eve"
+        nombre_cols[index_cols_eve[1]] <- "cod_eve"
+        nombre_cols <- nombre_cols[-(2:length(index_cols_eve))]
+      }
       data_event <- rbind(data_event, data_import[, nombre_cols])
     }
   }
@@ -214,7 +223,8 @@ import_sep_data <- function(path_data = NULL, cache = TRUE) {
       }
     }
     if (stringr::str_detect(file_name, ".xls")) {
-      data <- readxl::read_excel(file_path)
+      data <- readxl::read_excel(file_path,
+                                 col_types = "text")
     }
   }
   return(data)
