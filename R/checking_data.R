9d4757b858ff4742ddfe19b2f7d651497c0f831d
GeraldineGomez <avocadoenterprise@gmail.com>
2024-03-29 22:16:05 -0500

    chore(agrupar_semanaepi): updated the documentation and example with the parameter col_semanaepi
    Ref: #130

--- R/checking_data.R
+++ R/checking_data.R
@@ -132,12 +132,17 @@ obtener_casos_pob_especial <- function(data_event) {
 #' por semana epidemiológica y número de casos
 #' @param data_event Un `data.frame` que contiene los datos de
 #' una enfermedad o evento
+#' @param col_semanaepi Un `character` (cadena de caracteres)
+#' con el nombre de la columna que contiene las semanas
+#' epidemiológicas en los datos de la enfermedad o evento;
+#' su valor por defecto es `"semana"`
 #' @return Un `data.frame` con los datos de una enfermedad o
 #' evento agrupados por semana epidemiológica y número de casos
 #' @examples
 #' data(dengue2020)
 #' data_limpia <- limpiar_data_sivigila(data_event = dengue2020)
 #' agrupar_semanaepi(data_event = data_limpia,
+#'                   col_semanaepi = "semana")
 #' @export
 agrupar_semanaepi <- function(data_event,
                               col_semanaepi = "semana") {
