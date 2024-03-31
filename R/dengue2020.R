f9f5592ed47e22c9ee242be0d09f4a749657dfd5
GeraldineGomez <avocadoenterprise@gmail.com>
2024-03-31 05:46:46 -0500

    fix: added dengue 2020 and divipola data
    Ref: #130

---
+++ R/dengue2020.R
@@ -0,0 +1,15 @@
+#' Datos Dengue 2020 en sivirep
+#'
+#' Data from
+#'
+#' @docType data
+#'
+#' @usage dengue2020
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' dengue2020
+"dengue2020"
