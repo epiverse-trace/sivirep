7d9d3789de2483b17d01bd01d8fd545b7cf35f8d
GeraldineGomez <avocadoenterprise@gmail.com>
2024-04-13 11:45:12 -0500

    chore: improved documentation of dengue2020 dataset
    The documentation of dengue2020 dataset has been improved, adding the description of its rows and columns.
    
    Ref: #134

--- R/dengue2020.R
+++ R/dengue2020.R
@@ -1,15 +1,94 @@
-#' Datos Dengue 2020 en sivirep
+#' Datos Dengue 2020 del SIVIGILA en sivirep
 #'
-#' Data from
+#' Datos obtenidos del SIVIGILA
+#' (Sistema de Vigilancia en Salud Pública de Colombia)
 #'
 #' @docType data
 #'
-#' @usage dengue2020
+#' @usage data(dengue2020)
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#' @format ## `dengue2020`
+#' Un `<data.frame>` con 65535 filas y 74 columnas:
+#' \describe{
+#'   \item{partición}{Partición}
+#'   \item{consecutive}{Consecutivo}
+#'   \item{cod_eve}{Codigo del evento}
+#'   \item{fec_not}{Fecha de notificación}
+#'   \item{semana}{Semana}
+#'   \item{ano}{Año}
+#'   \item{cod_pre}{Código del prestador de servicios de salud}
+#'   \item{cod_sub}{Código del prestador de servicios de salud - subíndice}
+#'   \item{edad}{Edad}
+#'   \item{uni_med}{Unidad de medida de la edad}
+#'   \item{nacionalidad}{Nacionalidad}
+#'   \item{nombre_nacionalidad}{Nombre de la nacionalidad}
+#'   \item{sexo}{Sexo}
+#'   \item{cod_pais_o}{Código del país de ocurrencia}
+#'   \item{cod_dpto_o}{Código del departamento de ocurrencia}
+#'   \item{cod_mun_o}{Código del municipio de ocurrencia}
+#'   \item{area}{Área geográfica}
+#'   \item{ocupacion}{Ocupación del paciente}
+#'   \item{tip_ss}{Tipo de régimen en salud}
+#'   \item{cod_ase}{Código de administradora}
+#'   \item{per_etn}{Pertenencia étnica}
+#'   \item{gru_pob}{Grupo poblacional}
+#'   \item{nom_grupo}{Nombre del grupo poblacional}
+#'   \item{estrato}{Estrato socioeconómico}
+#'   \item{gp_discapa{Grupo poblacional - discapacitados}
+#'   \item{gp_desplaz}{Grupo poblacional - desplazados}
+#'   \item{gp_migrant}{Grupo poblacional - migrantes}
+#'   \item{gp_carcela}{Grupo poblacional - carcelarios}
+#'   \item{gp_gestan }{Grupo poblacional - gestantes}
+#'   \item{sem_ges}{semana gestante}
+#'   \item{gp_indigen}{Grupo poblacional - indigentes}
+#'   \item{gp_pobicfb}{Grupo poblacional - población infantil a cargo del ICBF}
+#'   \item{gp_mad_com}{Grupo poblacional - madres comunitarias}
+#'   \item{gp_desmovi}{Grupo poblacional - desmovilizados}
+#'   \item{gp_psiquia}{Grupo poblacional - población en centros psiquiátricos}
+#'   \item{gp_vic_vio}{Grupo poblacional - víctima de violencia}
+#'   \item{gp_otros}{Grupo poblacional - otros}
+#'   \item{fuente}{Fuente}
+#'   \item{cod_pais_r}{Código del país de residencia}
+#'   \item{cod_dpto_r}{Código del departamento de residencia}
+#'   \item{cod_mun_r}{Código del municipio de residencia}
+#'   \item{cod_dpto_n}{Código del departamento de notificación}
+#'   \item{cod_mun_n}{Código del municipio de notificación}
+#'   \item{fec_con}{Fecha de constitución o inicio de actividades asistenciales}
+#'   \item{ini_sin}{Fecha de inicio de síntomas}
+#'   \item{tip_cas}{Clasificación inicial del caso}
+#'   \item{pac_hos}{Hospitalizado}
+#'   \item{fec_hos}{Fecha de hospitalización}
+#'   \item{con_fin}{Condición final}
+#'   \item{fec_def}{Fecha de defunción}
+#'   \item{ajuste}{seguimiento y clasificación final del caso}
+#'   \item{fecha_nto}{Fecha de nacimiento}
+#'   \item{cer_def}{Número del certificado de defunción}
+#'   \item{cbmte}{Causa basica de defunción/muerte}
+#'   \item{fec_arc_xl}{Fecha de creación del archivo de Excel}
+#'   \item{fec_aju}{Fecha de ajuste}
+#'   \item{fm_fuerza}{Fuerza militar}
+#'   \item{fm_unidad}{Unidad - código de la unidad militar}
+#'   \item{fm_grado}{Grado - código del grado militar}
+#'   \item{confirmados}{Confirmados}
+#'   \item{consecutive_origen}{Consecutivo origen}
+#'   \item{va_sispro}{va_sispro - Sistema Integral de Información para
+#'   la Protección Social}
+#'   \item{estado_final_de_caso}{Estado final del caso}
+#'   \item{nom_est_f_caso}{nom_est_f_caso}
+#'   \item{nom_upgd}{Nombre de la unidad primaria generadora de dato}
+#'   \item{pais_ocurrencia}{País ocurrencia}
+#'   \item{nombre_evento}{Nombre evento}
+#'   \item{departamento_ocurrencia}{Departamento ocurrencia}
+#'   \item{municipio_ocurrencia}{Municipio ocurrencia}
+#'   \item{pais_residencia}{pais residencia}
+#'   \item{departamento_residencia}{Departamento residencia}
+#'   \item{municipio_residencia}{Municipio residencia}
+#'   \item{departamento_notificacion}{Departamento notificación}
+#'   \item{municipio_notificacion}{Municipio notificación}
+#' }
 #'
 #' @keywords datasets
 #'
 #' @examples
-#' dengue2020
+#' data(dengue2020)
 "dengue2020"
