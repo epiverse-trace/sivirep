4d062c4a4d2eeb186e66edc714ba2cff8242aa9b
GeraldineGomez <avocadoenterprise@gmail.com>
2024-04-19 09:52:38 -0500

    feat: created function that allows import the data incidence or population projections
    A new function has been created to import the data incidence or the population projections from DANE and its corresponding documentation.
    
    Ref: #108

--- R/import_data.R
+++ R/import_data.R
@@ -231,3 +231,41 @@ obtener_ruta_descarga <- function(ruta) {
                              "')", fixed = TRUE)[[1]][1] %>% as.character()
   return(nombre_archivo)
 }
+
+#' Importar las proyecciones DANE del año 2005 hasta el 2035
+#'
+#' Función que obtiene las proyecciones poblacionales DANE desde
+#' el año 2005 hasta el 2035
+#' @return Un `data.frame` con las proyecciones poblacionales
+#' @examples
+#' import_data_incidencia()
+#' @export
+import_data_incidencia <- function() {
+  proyecciones <- NULL
+  ruta_data <- config::get(file =
+                             system.file("extdata",
+                                         "config.yml",
+                                         package = "sivirep"),
+                           "incidence_data_paths")
+  for (ruta_inciden in ruta_data) {
+    extdata_path <- system.file("extdata", package = "sivirep")
+    ruta_archivo <- file.path(extdata_path, paste0("proyecciones",
+                                                ruta_inciden$year))
+    solicitud_archivo <- httr2::request(ruta_inciden$url)
+    respuesta_archivo <- httr2::req_perform(solicitud_archivo)
+    if (httr2::resp_status(respuesta_archivo) == 200) {
+      conten_archivo <- httr2::resp_body_raw(respuesta_archivo)
+      con_archivo <- file(ruta_archivo, "wb")
+      if (length(conten_archivo) > 0) {
+        writeBin(conten_archivo, con_archivo)
+      }
+      close(con_archivo)
+    }
+    proyecciones <- readxl::read_excel(ruta_archivo, skip = ruta_inciden$skip)
+    if (ruta_inciden$year == 2035) {
+      proyecciones <- dplyr::rename(proyecciones, total_general = .data$Total)
+    }
+    proyecciones <- rbind(proyecciones, proyecciones)
+  }
+  return(proyecciones)
+}
