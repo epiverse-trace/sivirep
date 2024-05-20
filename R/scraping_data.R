bac96cf1d7b8fc40a96d188d797f02c2b44a006d
GeraldineGomez <avocadoenterprise@gmail.com>
2024-05-20 11:45:39 -0500

    feat: translated function get_path_data_disease_year  to Spanish
    The function get_path_data_disease_year has been translated to Spanish with its corresponding documentation. Now it is called obtener_ruta_data_event_year.
    
    Ref: #179

--- R/scraping_data.R
+++ R/scraping_data.R
@@ -7,38 +7,38 @@
 #' @return Path or URL for download the disease data by a specific year of
 #' SIVIGILA source
 #' @keywords internal
-get_path_data_disease_year <- function(year, disease_name) {
-  config_file <- system.file("extdata", "config.yml", package = "sivirep")
-  base_path <- config::get(file = config_file, "base_path_microdata")
-  file_path <- config::get(file = config_file, "file_path_microdata")
-  file_path_parameters <- config::get(file = config_file,
-                                      "file_path_parameters_microdata")
-  microdata_path <- config::get(file = config_file, "path_microdata")
-  query_path <- config::get(file = config_file, "query_path_microdata")
+obtener_ruta_data_event_year <- function(nombre_event, year) {
+  archivo_config <- system.file("extdata", "config.yml", package = "sivirep")
+  ruta_base <- config::get(file = archivo_config, "base_path_microdata")
+  ruta_archivo <- config::get(file = archivo_config, "file_path_microdata")
+  ruta_archivo_params <- config::get(file = archivo_config,
+                                     "file_path_parameters_microdata")
+  ruta_microdata <- config::get(file = archivo_config, "path_microdata")
+  ruta_query <- config::get(file = archivo_config, "query_path_microdata")
   year <- as.character(year)
-  disease_name <- utils::URLencode(disease_name)
-  query_path <- stringr::str_replace(query_path, stringr::fixed("_year_"),
+  nombre_event <- utils::URLencode(nombre_event)
+  ruta_query <- stringr::str_replace(ruta_query, stringr::fixed("_year_"),
                                      year)
-  query_path <- stringr::str_replace(query_path, stringr::fixed("_disease_"),
-                                     disease_name)
-  query_disease_path <- paste0(base_path,
-                              paste0(microdata_path,
-                                    query_path))
-  query_disease_request <- httr2::request(query_disease_path)
-  query_disease_get <- httr2::req_perform(query_disease_request)
-  query_disease_response <- httr2::resp_body_string(query_disease_get)
-  response_document <- xml2::as_xml_document(query_disease_response)
-  property_file_ref <- xml2::xml_find_all(response_document, "//d:FileRef")
-  disease_file_ref <- xml2::xml_text(property_file_ref)
-  if (length(disease_file_ref) >= 2) {
-    disease_file_ref <- disease_file_ref[
-      which(stringr::str_detect(disease_file_ref,
+  ruta_query <- stringr::str_replace(ruta_query, stringr::fixed("_disease_"),
+                                     nombre_event)
+  ruta_query_event <- paste0(ruta_base,
+                             paste0(ruta_microdata,
+                                    ruta_query))
+  solicitud_query_event <- httr2::request(ruta_query_event)
+  get_query_event <- httr2::req_perform(solicitud_query_event)
+  respuesta_query_event <- httr2::resp_body_string(get_query_event)
+  respuesta_document <- xml2::as_xml_document(respuesta_query_event)
+  ref_archivo_propiedad <- xml2::xml_find_all(respuesta_document, "//d:FileRef")
+  ref_archivo_event <- xml2::xml_text(ref_archivo_propiedad)
+  if (length(ref_archivo_event) >= 2) {
+    ref_archivo_event <- ref_archivo_event[
+      which(stringr::str_detect(ref_archivo_event,
                                 stringr::fixed(year)))]
   }
-  file_path <- stringr::str_replace(file_path,
+  ruta_archivo <- stringr::str_replace(ruta_archivo,
                                     stringr::fixed("_filepath_"),
-                                    disease_file_ref)
-  file_download_path <- paste0(base_path,
-                               file_path, file_path_parameters)
-  return(file_download_path)
+                                    ref_archivo_event)
+  ruta_descarga_archivo <- paste0(ruta_base,
+                               ruta_archivo, ruta_archivo_params)
+  return(ruta_descarga_archivo)
 }
