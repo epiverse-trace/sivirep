e4c8012fa82fb4b2882bed1dfe066ee6265c7a93
GeraldineGomez <avocadoenterprise@gmail.com>
2024-07-12 22:40:15 -0500

    chore(agrupar_top_area_geo): data_event validation
    Ref: #194

--- R/checking_data.R
+++ R/checking_data.R
@@ -589,15 +589,11 @@ agrupar_top_area_geo <- function(data_event,
                              col_area = "area",
                              porcentaje = FALSE,
                              top = 10) {
-  stopifnot("El parametro data_event es obligatorio" = !missing(data_event),
-            "El parametro data_event debe ser un data.frame" =
-              is.data.frame(data_event),
-            "El parametro data_event no debe estar vacio" =
-              nrow(data_event) > 0,
             "El parametro col_area debe ser una cadena de caracteres"
             = is.character(col_area),
             "El parametro porcentaje debe ser un booleano (TRUE o FALSE)" =
               is.logical(porcentaje))
+  validar_data_event(data_event)
   nomb_cols <- append(col_area,
                       obtener_tip_ocurren_geo(data_event$cod_eve[1])[1:4])
   data_event_area <- data_event
