892f834aa889dde2771f3dc5a2b7de8318b7d792
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-07-23 09:57:52 +0200

    Copy infrastructure files from packagetemplate

---
+++ R/dev-utils.R
@@ -0,0 +1,11 @@
+# This unexported function adds a custom item to `usethis::use_release_issue()`
+release_bullets <- function() {
+
+  c(
+    "Run `goodpractice::gp()`",
+    "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
+    "Check if `# nolint` comments are still needed with recent lintr releases",
+    "All contributors to this release are acknowledged in some way"
+  )
+
+}
